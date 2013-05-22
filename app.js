89380f3f2dd901885c2ee7a3389fbf89297311df
Erick Ruiz de Chavez <erickrdch@gmail.com>
2013-05-22 19:22:02 -0400

    Adding layout.hjs and a very basic index.hjs and '/' route for testing for #2

--- app.js
+++ app.js
@@ -16,12 +16,15 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hjs');
 app.use(express.favicon());
-app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(partials());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.render('index.hjs');
+});
 
 http.createServer(app).listen(app.get('port'), function () {
   log.info('Express server listening on port ' + app.get('port'));
