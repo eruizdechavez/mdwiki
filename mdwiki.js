94f49bcdb1c7c1b45d550ef43527d5d9cd82b521
Erick Ruiz de Chavez <erickrdch@gmail.com>
2013-05-22 20:41:05 -0400

    Updated to be a "command". Closes #3.

--- mdwiki.js
+++ mdwiki.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+program
+  .option('-p, --port [port]', 'Port [8080]')
+  .parse(process.argv);
+
 var express = require('express'),
   partials = require('express-partials'),
   http = require('http'),
@@ -5,14 +12,12 @@ var express = require('express'),
   bunyan = require('bunyan');
 
 var app = express(),
-  log = bunyan.createLogger({
+  log = app.log = bunyan.createLogger({
     name: 'mdwiki'
-  });
+  }),
+  _path = path.resolve(program.args.shift() || '.');
 
-app.log = log;
-
-// all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', program.port || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hjs');
 app.use(express.favicon());
@@ -21,11 +26,13 @@ app.use(express.methodOverride());
 app.use(partials());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(_path));
 
 app.get('/', function (req, res) {
   res.render('index.hjs');
 });
 
 http.createServer(app).listen(app.get('port'), function () {
-  log.info('Express server listening on port ' + app.get('port'));
+  log.info('mdwiki running on port ' + app.get('port'));
+  log.info('mdwiki serving content at ' + _path);
 });
